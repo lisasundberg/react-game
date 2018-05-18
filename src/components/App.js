0ae3532df4621a3679f17ea89d0c1ec8c471a3e1
Lisa Sundberg <lisa.sundberg@hyperisland.se>
2018-05-18 13:45:50 +0200

    Create header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import Header from './Header';
+import Container from './Container';
 import './../css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header>
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          <h1 className="App-title">En KLICK guaca tack</h1>
+        </Header>
+        <Container>
+          <p className="App-intro">
+            Klicka på mig!
+            GUACA ÄR MÅLET
+          </p>
+        </Container>
+
       </div>
     );
   }
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function Container(props){
-  return(){
+  return(
     <div className="container">
       { props.children }
     </div>
-  }
+  );
 }
 
 export default Container;

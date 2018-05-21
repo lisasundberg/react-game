13ac788af14f8dd69e5a3caedbc291d44904cbbe
Lisa Sundberg <lisa.sundberg@hyperisland.se>
2018-05-21 14:43:55 +0200

    First draft of upgrade-functionality

--- src/components/App.js
+++ src/components/App.js
@@ -11,28 +11,50 @@ class App extends Component {
   state = {
     name: '',
     counter: 0,
-    popupVisible: true
+    popupVisible: false,
+    upgradeOneVisible: false,
+    upgradeTwoVisible: false,
+    upgradeThreeVisible: false,
+    upgradeFourVisible: false,
+    upgradeFiveVisible: false
   }
-
-  // hide = (element) => {
-  //   element.classList.add("hidden");
-  // }
 
   handleClick = () => {
     this.setState({ counter: this.state.counter + 1 });
+
+    if(this.state.counter === 4) {
+      this.setState({ upgradeOneVisible: true });
+      console.log('You get an onion');
+    }
+    if(this.state.counter === 10) {
+      this.setState({ upgradeTwoVisible: true });
+      console.log('You get a tomato')
+    }
+    if(this.state.counter === 15) {
+      this.setState({ upgradeThreeVisible: true });
+      console.log('You get a garlic clove')
+    }
+    if(this.state.counter === 20) {
+      this.setState({ upgradeFourVisible: true });
+      console.log('You get a chili')
+    }
+    if(this.state.counter === 25) {
+      this.setState({ upgradeFiveVisible: true });
+      console.log('You get an avocado! Congratulations! You made it!')
+    }
   }
+
   saveName = (event) => {
-    this.setState({ name: this.username.value, popupVisible: false });
-    // let popup = document.getElementById('popup');
-    // console.log(this.popup);
-    // this.hide(popup);
+    this.setState({
+      name: this.username.value,
+      popupVisible: false
+    });
   }
 
   render() {
     return (
       <div className="App">
         <Header>
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
           <h1 className="App-title">En klick guaca tack</h1>
           <p className="username">{ this.state.name }</p>
         </Header>
@@ -40,15 +62,24 @@ class App extends Component {
           { this.state.popupVisible &&
             <Popup>
               <label htmlFor="username">Vad heter du?</label>
-              <input type="text" name="username" ref={ input => this.username = input } />
+              <input type="text" name="username" ref={ input => this.username = input } autoComplete="off"/>
               <button onClick={ this.saveName }> OK GO </button>
-            </Popup> }
+            </Popup>
+          }
           <h2>
             { this.state.counter }
           </h2>
           <Button handleClick={ this.handleClick }>
             <p>Click me!</p>
           </Button>
+          <div className="upgrades">
+            { this.state.upgradeOneVisible && <div className="upgrade upgrade_one"></div> }
+            {  this.state.upgradeTwoVisible && <div className="upgrade upgrade_two"></div> }
+            {  this.state.upgradeThreeVisible && <div className="upgrade upgrade_three"></div> }
+            {  this.state.upgradeFourVisible && <div className="upgrade upgrade_four"></div> }
+            {  this.state.upgradeFiveVisible && <div className="upgrade upgrade_five"></div> }
+
+          </div>
         </Container>
 
       </div>

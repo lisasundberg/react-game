a848f7d501ef4c13786867363d81ef57d92b3581
Lisa Sundberg <lisa.sundberg@hyperisland.se>
2018-05-22 15:00:08 +0200

    Add animation to feedback

---
+++ src/components/Feedback.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Feedback(props) {
+    return(
+      <div className="feedback">
+        { props.children }
+      </div>
+    );
+}
+
+export default Feedback;
